36a112a39a99a327a9b3017cbb32207951d14a96
andrea capizzi <andreaitalia2020@libero.it>
2021-04-03 20:15:19 +0200

    bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,49 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+// import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
-function Navbar() {
-  return (
-    <div>
-      <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            ALADIN
-            <i class="fab fa-typo3" />
-          </Link>
-        </div>
-      </nav>
-    </div>
-  );
+// import Form from "react-bootstrap/Form";
+// import FormControl from "react-bootstrap/FormControl";
+// import Button from "react-bootstrap/Button";
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+        <Navbar.Brand>
+          <p>
+            MVV <i class="fab fa-typo3" />
+          </p>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            {/* <Link to="/">Home</Link>
+            <Link to="/">Home</Link>
+            <Link to="/">Home</Link> */}
+            <LinkContainer to="/service">
+              <Nav.Link href="/">Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/service">
+              <Nav.Link href="/">Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/service">
+              <Nav.Link href="/">Home</Nav.Link>
+            </LinkContainer>
+            
+            <NavDropdown.Divider />
+          </Nav>
+          {/* <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form> */}
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
 
-export default Navbar;
+export default NavBar;

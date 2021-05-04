f8b4f8f5c663cd611bc5685295e1937d15bf788a
andrea capizzi <andreaitalia2020@libero.it>
2021-05-04 19:15:05 +0200

    S

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,9 @@
 import React from "react";
-// import { Link } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
-
 import Form from "react-bootstrap/Form";
-// import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 import "./Navbar.css";
 
@@ -14,38 +11,34 @@ class NavBar extends React.Component {
   render() {
     return (
       <nav>
-        <Navbar className="nvbar_my" variant="dark" expand="lg" fixed="top">
-          <Navbar.Brand>
-            <p>
-              <i className="fab fa-affiliatetheme fa-1x"></i>
-            </p>
-          </Navbar.Brand>
+        <Navbar className="myclass" variant="dark" bg="light" expand="lg">
+          <Navbar.Brand href="#home">
+            <img
+              src="images/logo3.png"
+              width="60"
+              height="60"
+              className="d-inline-block align-top"
+              alt="images/logo3.png"
+            />
+          </Navbar.Brand> 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
               <LinkContainer to="/">
                 <Nav.Link>Home</Nav.Link>
               </LinkContainer>
-
               <Nav.Link href="#about">About</Nav.Link>
               <Nav.Link href="#destinations">Destinations</Nav.Link>
-
               <Nav.Link href="#news">News</Nav.Link>
-              <LinkContainer disabled to="/services">
-                <Nav.Link>Services</Nav.Link>
-              </LinkContainer>
-
-              <NavDropdown.Divider />
             </Nav>
-
             <LinkContainer to="/SignUp">
               <Form inline>
                 <Button variant="outline-warning">Sign Up</Button>
               </Form>
             </LinkContainer>
-   
           </Navbar.Collapse>
         </Navbar>
+       
       </nav>
     );
   }
